e2354270eebed28a1450e780b29a29f1c248168c
martinhelmutda <martinhelmutmh@gmail.com>
2025-06-14 23:33:06 -0600

    Implement team-members/ endpoints. Swagger added for API documentation

--- team/models.py
+++ team/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TeamMember(AbstractUser):
+    """
+    Custom TeamMember model that extends Django's AbstractUser.
+    This model includes additional fields for phone number.
+
+    """
+
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return f"{self.get_full_name()} ({self.role})"
+
+    class Meta:
+        verbose_name = "Team Member"
+        verbose_name_plural = "Team Members"
+        ordering = ["username"]
---
+++ team/urls.py
@@ -0,0 +1,25 @@
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+from rest_framework.routers import DefaultRouter
+
+from .views import TeamMemberViewSet
+
+router = DefaultRouter()
+router.register(r"team-members", TeamMemberViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
+]
