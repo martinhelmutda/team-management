5f551826683d7853243b4efd3dea8aba43dc87be
martinhelmutda <martinhelmutmh@gmail.com>
2025-06-15 21:45:42 -0600

    Solve issues setting groups a.k.a roles

--- team/serializers.py
+++ team/serializers.py
@@ -11,7 +11,7 @@ class TeamMemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = TeamMember
         fields = "__all__"
-        read_only_fields = ["id", "date_joined", "last_login", "username"]
+        read_only_fields = ["id", "date_joined", "last_login", "username", "user_permissions"]
         extra_kwargs = {"password": {"write_only": True, "required": False},}
 
     def create(self, validated_data):
@@ -19,9 +19,12 @@ class TeamMemberSerializer(serializers.ModelSerializer):
         Create a new TeamMember instance with the provided validated data.
         The password is hashed before saving.
         """
+        groups = validated_data.pop("groups", None)
         user = TeamMember(**validated_data)
         password = validated_data.get("password") or validated_data.get("phone_number")
         user.username = validated_data.get("email")
         user.set_password(password)
         user.save()
+        if groups:
+            user.groups.set(groups)
         return user
