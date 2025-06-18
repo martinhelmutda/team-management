2e060535623120550442da7561145bf43719d1d7
martinhelmutda <martinhelmutmh@gmail.com>
2025-06-18 02:06:58 -0600

    Remove autohide notifications

--- frontend/src/context/NotificationContext.tsx
+++ frontend/src/context/NotificationContext.tsx
@@ -11,15 +11,11 @@ type Notification = {
 type NotificationContextType = {
     notification: Notification | null;
     setNotification: (notif: Notification | null) => void;
-    // showNotification: (message: string, type?: NotificationType) => void;
-    // hideNotification: () => void;
 };
 
 const NotificationContext = createContext<NotificationContextType>({
     notification: null,
     setNotification: () => { },
-    // showNotification: () => { },
-    // hideNotification: () => { },
 });
 
 export function useNotification() {
@@ -29,26 +25,10 @@ export function useNotification() {
 export const NotificationProvider = ({ children }: { children: React.ReactNode }) => {
     const [notification, setNotification] = useState<Notification | null>(null);
 
-
-    // const showNotification = (message: string, type: NotificationType = 'success') => {
-    //     setNotification({ message, type });
-
-    //     // Auto-hide after 4 seconds
-    //     setTimeout(() => {
-    //         setNotification(null);
-    //     }, 4000);
-    // };
-
-    // const hideNotification = () => {
-    //     setNotification(null);
-    // };
-
     return (
         <NotificationContext.Provider value={{
             notification,
             setNotification
-            // showNotification,
-            // hideNotification
         }}>
             {children}
         </NotificationContext.Provider>
